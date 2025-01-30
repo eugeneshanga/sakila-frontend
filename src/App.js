a294033d8caf2a6a16065add6a467a32fba8fcc2
Eugene Shanga <es472@njit.edu>
2025-01-29 21:30:26 -0500

    Frontend setup 'Hello World'

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/health')
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{message}</h1> {/* Display API response */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
